7deef1c1eb9b268b5a716c70e1696d8a9ca2c699
Vlad <dog792804@gmail.com>
2023-12-04 11:15:05 +0200

    some fixes

--- SYSLAB5Taran/Program.cs
+++ SYSLAB5Taran/Program.cs
@@ -1,219 +1,4 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Text.RegularExpressions;
-//using System.IO;
-
-//class LexicalAnalyzer
-//{
-//    static void Main(string[] args)
-//    {
-//        string filePath = "C:\\Users\\dog79\\source\\repos\\SYSLAB5Taran\\SYSLAB5Taran\\index.txt";
-//        string code;
-
-//        try
-//        {
-//            code = File.ReadAllText(filePath);
-//        }
-//        catch (Exception ex)
-//        {
-//            Console.WriteLine($"Error reading file: {ex.Message}");
-//            return;
-//        }
-
-//        List<Token> tokens = Analyze(code);
-
-//        foreach (Token token in tokens)
-//        {
-//            Console.WriteLine($"< {token.Value}, {token.Type} >");
-//        }
-
-
-//    }
-
-
-
-//    public enum TokenType
-//    {
-//        Number,
-//        String,
-//        Operator,
-//        Identifier,
-//        Directive,
-//        Comment,
-//        Reserved,
-//        Delimiter,
-//        Error
-//    }
-
-//    public class Token
-//    {
-//        public string Value { get; }
-//        public TokenType Type { get; }
-
-//        public Token(string value, TokenType type)
-//        {
-//            Value = value;
-//            Type = type;
-//        }
-//    }
-
-//    public static List<Token> Analyze(string code)
-//    {
-//        List<Token> tokens = new List<Token>();
-//        HashSet<string> declaredVariables = new HashSet<string>();
-//        bool expectingIdentifierOrString = false;
-//        bool isInString = false;
-//        bool isInDirective = false;
-//        string currentString = "";
-
-//        string directivePattern = @"^#(\w+)";
-//        string commentPattern = @"//.*";
-//        string reservedWordsPattern = @"\b(abstract|as|base|bool|break|byte|case|catch|char|checked|class|const|continue|decimal|default|delegate|do|double|else|enum|event|explicit|extern|false|finally|fixed|float|for|foreach|goto|if|implicit|int|interface|internal|is|lock|long|namespace|new|null|object|operator|out|override|params|private|protected|public|readonly|ref|return|sbyte|sealed|short|sizeof|stackalloc|static|string|struct|switch|this|throw|true|try|typeof|uint|ulong|unchecked|unsafe|ushort|using|using\sstatic|virtual|void|volatile|while)\b";
-//        string delimiterPattern = @"[,.;()\[\]{}]";
-
-//        string[] lines = code.Split('\n');
-
-//        foreach (string line in lines)
-//        {
-//            bool isComment = false;
-//            string[] words = Regex.Split(line, @"(\s+|" + delimiterPattern + @")");
-
-//            for (int i = 0; i < words.Length; i++)
-//            {
-//                string word = words[i];
-//                if (string.IsNullOrWhiteSpace(word))
-//                {
-//                    continue;
-//                }
-
-//                if (isInDirective)
-//                {
-//                    tokens.Add(new Token(word, TokenType.Directive));
-//                    isInDirective = false;
-//                    continue;
-//                }
-
-//                if (word.StartsWith("#"))
-//                {
-//                    isInDirective = true;
-//                    tokens.Add(new Token(word, TokenType.Directive));
-//                    continue;
-//                }
-
-//                if (isInString)
-//                {
-//                    currentString += word;
-//                    if (word.EndsWith("\""))
-//                    {
-//                        tokens.Add(new Token(currentString, TokenType.String));
-//                        isInString = false;
-//                        currentString = "";
-//                    }
-//                    continue;
-//                }
-
-//                if (word.StartsWith("\""))
-//                {
-//                    isInString = true;
-//                    currentString = word;
-//                    if (word.EndsWith("\""))
-//                    {
-//                        tokens.Add(new Token(currentString, TokenType.String));
-//                        isInString = false;
-//                        currentString = "";
-//                    }
-//                    continue;
-//                }
-
-//                if (isComment)
-//                {
-//                    tokens.Add(new Token(word, TokenType.Comment));
-//                    continue;
-//                }
-
-//                TokenType type = TokenType.Error;
-
-//                if (Regex.IsMatch(word, directivePattern))
-//                {
-//                    type = TokenType.Directive;
-//                }
-//                else if (Regex.IsMatch(word, commentPattern))
-//                {
-//                    type = TokenType.Comment;
-//                    isComment = true;
-//                }
-//                else if (Regex.IsMatch(word, delimiterPattern))
-//                {
-//                    type = TokenType.Delimiter;
-//                }
-//                else if (IsNumber(word))
-//                {
-//                    type = TokenType.Number;
-//                }
-//                else if (IsOperator(word))
-//                {
-//                    type = TokenType.Operator;
-//                }
-//                else if (expectingIdentifierOrString)
-//                {
-//                    if (IsIdentifier(word))
-//                    {
-//                        declaredVariables.Add(word);
-//                        type = TokenType.Identifier;
-//                    }
-//                    else
-//                    {
-//                        Console.WriteLine($"Error: Expected identifier or string after type, found {word}");
-//                        type = TokenType.Error;
-//                    }
-//                    expectingIdentifierOrString = false;
-//                }
-//                else if (Regex.IsMatch(word, reservedWordsPattern))
-//                {
-//                    type = TokenType.Reserved;
-//                    expectingIdentifierOrString = true;
-//                }
-//                else if (IsIdentifier(word))
-//                {
-//                    if (declaredVariables.Contains(word))
-//                    {
-//                        type = TokenType.Identifier;
-//                    }
-//                    else
-//                    {
-//                        type = TokenType.Error;
-//                        Console.WriteLine($"Error: Undeclared variable {word}");
-//                    }
-//                }
-
-//                if (!isInString)
-//                {
-//                    tokens.Add(new Token(word, type));
-//                }
-//            }
-//        }
-
-//        return tokens;
-//    }
-
-//    private static bool IsNumber(string word)
-//    {
-//        return Regex.IsMatch(word, @"^\d+(\.\d+)?(f|F)?|0[xX][0-9a-fA-F]+|0[oO][0-7]+|0[bB][01]+$");
-//    }
-
-//    private static bool IsOperator(string word)
-//    {
-//        return Regex.IsMatch(word, @"^(\+|-|\*|/|%|=|<|>|!|&|\||\(|\)|{|}|,|\[|\]|\.|\;)$");
-//    }
-
-//    private static bool IsIdentifier(string word)
-//    {
-//        return Regex.IsMatch(word, @"^[a-zA-Z_]\w*$");
-//    }
-//}
-
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using System.IO;
@@ -224,18 +9,8 @@ class LexicalAnalyzer
 {
     static void Main(string[] args)
     {
-        string filePath = "C:\\Users\\dog79\\source\\repos\\SYSLAB5Taran\\SYSLAB5Taran\\index.txt"; 
-        string code;
-
-        try
-        {
-            code = File.ReadAllText(filePath);
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Error reading file: {ex.Message}");
-            return;
-        }
+        Console.WriteLine("Please enter the code:");
+        string code = Console.ReadLine();
 
         List<Token> tokens = Analyze(code);
 
@@ -244,7 +19,7 @@ class LexicalAnalyzer
             Console.WriteLine($"< {token.Value}, {token.Type} >");
         }
 
-        // Обчислення виразів
+        // Extract and evaluate expressions
         string expression = ExtractExpression(tokens);
         if (!string.IsNullOrWhiteSpace(expression))
         {
@@ -259,11 +34,10 @@ class LexicalAnalyzer
             }
         }
 
-
+        // Build and print AST
         try
         {
             AstNode ast = BuildAst(expression);
-            // Візуалізація AST
             ast.Print();
         }
         catch (Exception ex)
